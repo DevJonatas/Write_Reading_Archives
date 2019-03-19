60c890dd18ec2c91cf5624ce0fb7904d5e288d6e
DevJonatas <jonatas@workmail.com>
2019-03-19 19:22:06 -0300

    #commit add variable nameArchive

--- Write_Reading.py
+++ Write_Reading.py
@@ -1,17 +1,18 @@
 
+nameArchive = "archive.txt"
 
 def Write(word):
-    archive = open("archive.txt","w")
+    archive = open(nameArchive,"w")
     archive.write(word + "{}".format("\n"))
     archive.close()
 
 def AddWord(word):
-    archive = open("archive.txt","a")
+    archive = open(nameArchive,"a")
     archive.write(word+"{}".format("\n"))
     archive.close()
 
 def Reading():
-    archive = open("archive.txt","r")
+    archive = open(nameArchive,"r")
     listWords = []
     for words in archive:
         listWords.append(words.strip())
